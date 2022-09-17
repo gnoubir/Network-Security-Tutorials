ec6c78a117c7f80f9847451e8ec38ecbc1e499c9
Guevara Noubir <g.noubir@northeastern.edu>
2022-09-17 16:39:48 -0400

    pretify

--- ZeroMQ/zmq-example-1-client.py
+++ ZeroMQ/zmq-example-1-client.py
@@ -12,7 +12,7 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Connecting to hello world server...")
 socket = context.socket(zmq.REQ)
-#socket.connect("tcp://localhost:5555")
+# socket.connect("tcp://localhost:5555")
 socket.bind("tcp://*:5555")
 
 #  Do 10 requests, waiting each time for a response
