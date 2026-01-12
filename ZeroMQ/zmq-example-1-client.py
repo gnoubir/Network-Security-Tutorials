a790e8034d17cfd419ef622b2b30bc21fa05d63c
Guevara Noubir <g.noubir@northeastern.edu>
2026-01-12 10:50:57 -0500

    swap client with server zmq socket binding

--- ZeroMQ/zmq-example-1-client.py
+++ ZeroMQ/zmq-example-1-client.py
@@ -12,8 +12,8 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Connecting to hello world server...")
 socket = context.socket(zmq.REQ)
-# socket.connect("tcp://localhost:5555")
-socket.bind("tcp://*:5555")
+socket.connect("tcp://localhost:5555")
+#socket.bind("tcp://*:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
--- ZeroMQ/zmq-example-1-server.py
+++ ZeroMQ/zmq-example-1-server.py
@@ -11,8 +11,8 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-#socket.bind("tcp://*:5555")
-socket.connect("tcp://localhost:5555")
+socket.bind("tcp://*:5555")
+#socket.connect("tcp://localhost:5555")
 
 while True:
     #  Wait for next request from client
